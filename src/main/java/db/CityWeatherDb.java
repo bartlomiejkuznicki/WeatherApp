4bc1628337a657740daa4e67cf76af41d32d3a08
bokuz <bokuznicki1@gmail.com>
2023-11-21 20:59:26 +0100

    First

--- src/main/java/db/CityWeatherDb.java
+++ src/main/java/db/CityWeatherDb.java
@@ -7,7 +7,23 @@ public class CityWeatherDb {
     private static final Map<Long, CityDataEnity> dataBase = new HashMap<>();
 
     // TODO: metody do pracy nad bazą danych, dodawanie itp...
-     public void  add(CityDataEnity enity) {
-         dataBase.put(enity.getId(), enity);
-     }
+    public void add(CityDataEnity enity) {
+        dataBase.put(enity.getId(), enity);
+    }
+
+    public CityDataEnity change(Long id, String name, WeatherDataEntity weatherDataEntity) {
+//logika
+        return dataBase.get(id);
+
+    }
+
+
+    public CityDataEnity change(CityDataEnity cityDataEnity) {
+        final CityDataEnity toChange = dataBase.get(cityDataEnity.getId());
+        toChange.setName(cityDataEnity.getName());
+        toChange.setWeatherDataEntity(cityDataEnity.getWeatherDataEntity());
+        return  dataBase.put(cityDataEnity.getId(), toChange);
+        //wyszukac po id
+        //zmienic wartosc w obiekcie na takie jakie dostaje metoda
+    }
 }
---
+++ src/test/java/db/CityWeatherDbTest.java
@@ -0,0 +1,10 @@
+package db;
+
+import org.junit.jupiter.api.Test;
+
+class CityWeatherDbTest {
+
+    @Test
+    void add() {
+    }
+}
